164b29580dab96cd6e62f772379384cd7cb48a28
Leonardo Mejia <leonardomn1203@gmail.com>
2020-08-11 23:27:24 -0500

    Adelanto codigo guia 1

---
+++ src/main/java/Modelo/Usuario.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author leome
+ */
+public class Usuario {
+    String nombre;
+   private String contraseña;
+
+    public Usuario(String nombre, String contraseña) {
+        this.nombre = nombre;
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+   
+    
+    
+}
